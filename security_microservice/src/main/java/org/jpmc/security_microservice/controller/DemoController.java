03c1ada3af7348876acbb09911b910e0aab4a4dc
Rakshith Gowda <rakshithgowda0710@gmail.com>
2025-06-16 13:11:23 +0530

    Refactor DemoController.java to improve code readability by adding comments for each endpoint

--- security_microservice/src/main/java/org/jpmc/security_microservice/controller/DemoController.java
+++ security_microservice/src/main/java/org/jpmc/security_microservice/controller/DemoController.java
@@ -9,45 +9,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/demo")
 public class DemoController {
     
+    // This is a demo controller to test the security microservice
     @GetMapping("/greet")
     public String demo1() {
         return "Hello, this is a demo endpoint1!";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/rxith")
     public String greetMe(){
         return "Rakshith Updating Something";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromsonu")
     public String demo2() {
         return "Hello, this is a demo endpoint2 from sonu!";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromsonu")
     public String demo300() {
         return "Hello, this is a demo for to raise conflict!";
     }
     
+    // This is a demo controller to test the security microservice
     @GetMapping("/greet-by-leela")
     public String demoByLeela() {
         return "Hello, this is a demo endpoint!";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromsheela")
     public String demo3() {
         return "Hello, this is a demo endpoint3!";
     }
+
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromsheela1")
     public String test() {
         return "Hello, this is a demo endpoint12!";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromaishwarya")
     public String demo12() {
         return "Hello, this is a demo endpoint12!";
     }
 
+    // This is a demo controller to test the security microservice
     @GetMapping("/greetfromaishwarya1")
     public String demo14() {
         return "Hello, this is a demo endpoint12!";
