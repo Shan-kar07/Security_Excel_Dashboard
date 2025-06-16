fbb503c20dd2a29c59c69924adb98e615b60b7ab
Rakshith Gowda <rakshithgowda0710@gmail.com>
2025-06-16 12:38:50 +0530

    Add missing newline for improved code readability in DemoController

--- security_microservice/src/main/java/org/jpmc/security_microservice/controller/DemoController.java
+++ security_microservice/src/main/java/org/jpmc/security_microservice/controller/DemoController.java
@@ -1,4 +1,5 @@
 package org.jpmc.security_microservice.controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
